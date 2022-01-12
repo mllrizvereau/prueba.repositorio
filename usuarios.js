2984bfeec4d154bac0375581f1a6b461a39505ab
mllerizvereau <marifelrivero16@gmail.com>
2022-01-12 16:43:37 -0300

    Puse datos en los archivos

---
+++ usuarios.js
@@ -0,0 +1,16 @@
+[
+    {
+        nombre : "Lucas",
+        puesto : "tech leader"
+
+    },{
+        nombre : "Gonza",
+        puesto : "tech leader"
+    },{
+        nombre : "Marifel",
+        puesto : "Dev"
+    },{
+        nombre : "Cristian",
+        puesto : "Dev"
+    }
+]
